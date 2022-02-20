6a26b252b083287bfb394d580cb44919dd38cc97
Shah rahman <toshah.rahman@gmail.com>
2022-02-20 20:53:32 +0200

    Added assertinons to validate response

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -6,8 +6,16 @@ test.describe.parallel('API Testing', () => {
   test('Simple API Test - Assert Response Status', async ({ request }) => {
     const response = await request.get(`${baseUrl}/users/3`)
     expect(response.status()).toBe(200)
-
+    //asserting the response
     const responseBody = JSON.parse(await response.text())
+    console.log(responseBody)
+    expect(responseBody.data.id).toBe(3)
+    expect(responseBody.data.first_name).toBe('Emma')
+    expect(responseBody.data.last_name).toBe('Wong')
+    expect(responseBody.data.email).toBeTruthy()
+    expect(responseBody.support.url).toBe('https://reqres.in/#support-heading')
+    expect(responseBody.support.text).toBe('To keep ReqRes free, contributions towards server costs are appreciated!')
+
   })
   test('Simple API Test - Assert Invalid Endpoint', async ({ request }) => {
     const response = await request.get(`${baseUrl}/users/non-existing-endpoint`)
