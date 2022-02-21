b62e78a5f53e8dada338b75927bf2e2809a418e3
Shah rahman <toshah.rahman@gmail.com>
2022-02-21 07:02:37 +0200

    api post requset and response assert

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -21,4 +21,16 @@ test.describe.parallel('API Testing', () => {
     const response = await request.get(`${baseUrl}/users/non-existing-endpoint`)
     expect(response.status()).toBe(404)
   })
+  test('POST Request - Create New User', async ({ request }) => {
+    const response = await request.post(`${baseUrl}/user`, {
+      data: {
+        id: 1000,
+      },
+    })
+    const responseBody = JSON.parse(await response.text())
+    expect(responseBody.id).toBe(1000)
+    expect(responseBody.createdAt).toBeTruthy()
+  })
+  
+
 })
