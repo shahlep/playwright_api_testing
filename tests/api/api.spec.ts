470efcd256e71fa422e750b41e763d340d47919f
Shah rahman <toshah.rahman@gmail.com>
2022-02-21 17:36:35 +0200

    DELETE request and response status code assertions

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -68,4 +68,9 @@ test.describe.parallel('API Testing', () => {
     expect(responseBody.updatedAt).toBeTruthy()
   })
 
+  test('DELETE Request - Delete User', async ({ request }) => {
+    const response = await request.delete(`${baseUrl}/users/2`)
+    expect(response.status()).toBe(204)
+  })
+
 })
