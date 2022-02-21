62014e63082eebc495c44d7ecd976d800decd1f2
Shah rahman <toshah.rahman@gmail.com>
2022-02-21 17:20:45 +0200

    POST request with logi fail and response assertions

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -42,5 +42,15 @@ test.describe.parallel('API Testing', () => {
     expect(response.status()).toBe(200)
     expect(responseBody.token).toBeTruthy()
   })
+  test('POST Request - Login Fail', async ({ request }) => {
+    const response = await request.post(`${baseUrl}/login`, {
+      data: {
+        email: 'eve.holt@reqres.in',
+      },
+    })
+    const responseBody = JSON.parse(await response.text())
+    expect(response.status()).toBe(400)
+    expect(responseBody.error).toBe('Missing password')
+  })
 
 })
